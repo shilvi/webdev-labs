bf9b295d9e2cac9068fb6111e29d41cfac34f4e6
Shilvi Satpati <rimli.satpati@gmail.com>
2022-02-02 22:36:31 -0600

    Finished lab05 Exercise 1

--- lab05/01-font-switcher/index.js
+++ lab05/01-font-switcher/index.js
@@ -1,12 +1,19 @@
+const SCALE_FACTOR = 1.2
+
+const getFontSize = (selector) =>
+   parseFloat(window.getComputedStyle(document.querySelector(selector)).fontSize)
+
 const makeBigger = () => {
-   alert('make bigger!');
+   for (selector of ['h1', '.content']) {
+      document.querySelector(selector).style.fontSize = `${getFontSize(selector) * SCALE_FACTOR}px`
+   }
 };
 
 const makeSmaller = () => {
-   alert('make smaller!');
+   for (selector of ['h1', '.content']) {
+      document.querySelector(selector).style.fontSize = `${getFontSize(selector) / SCALE_FACTOR}px`
+   }
 };
 
-/*
-document.querySelector(???).addEventListener('click', makeBigger);
-document.querySelector(???).addEventListener('click', makeSmaller);
-*/
+document.querySelector('#a1').addEventListener('click', makeBigger);
+document.querySelector('#a2').addEventListener('click', makeSmaller);
