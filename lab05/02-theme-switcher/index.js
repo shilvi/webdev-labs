31fc1df514c266a26abba6a4771e90c710941d2a
Shilvi Satpati <rimli.satpati@gmail.com>
2022-02-02 22:54:22 -0600

    Finished lab05 Exercise 2

--- lab05/02-theme-switcher/index.js
+++ lab05/02-theme-switcher/index.js
@@ -1,7 +1,5 @@
-/*
-    Hints: 
-    1. Attach click event handlers to all four of the 
-       buttons (#default, #ocean, #desert, and #high-contrast).
-    2. Modify the className property of the body tag 
-       based on the button that was clicked.
-*/
+document.querySelectorAll('button').forEach((element) => {
+   element.addEventListener('click', function(event) {
+      document.querySelector('body').className = event.target.id
+   })
+})
