76194f58bd55ad32731e6c850e42435d5a1e154c
Shilvi Satpati <rimli.satpati@gmail.com>
2022-02-03 22:29:52 -0600

    Finished lab05 Exercise 3

--- lab05/03-dyslexia-mode/index.js
+++ lab05/03-dyslexia-mode/index.js
@@ -1,4 +1,22 @@
-/* 
-  See Smashing Magazine Tutorial:
-  https://www.smashingmagazine.com/2021/11/dyslexia-friendly-mode-website/
-*/
+const DYSLEXIA_MODE_TAG = 'dyslexia-mode'
+
+document.addEventListener('DOMContentLoaded', function() {
+  dyslexiaMode = localStorage.getItem(DYSLEXIA_MODE_TAG)
+  if (dyslexiaMode === null) {
+    localStorage.setItem(DYSLEXIA_MODE_TAG, 'default')
+  }
+  else {
+    document.querySelector('body').className = dyslexiaMode
+  }
+})
+
+document.querySelector('button').addEventListener('click', function() {
+  if (localStorage.getItem(DYSLEXIA_MODE_TAG) === 'default') {
+    localStorage.setItem(DYSLEXIA_MODE_TAG, 'dyslexia-mode')
+    document.querySelector('body').className = 'dyslexia-mode'
+  }
+  else {
+    localStorage.setItem(DYSLEXIA_MODE_TAG, 'default')
+    document.querySelector('body').className = 'default'
+  }
+})
